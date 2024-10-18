31475da141ae8d0168955b0ba5826a36a6bbea98
Adit <adityonugroho68@gmail.com>
2024-10-18 20:23:42 +0700

    Initial commit

---
+++ app/src/main/java/com/example/tugas3/DatabaseHelper.java
@@ -0,0 +1,41 @@
+package com.example.tugas3;
+import android.content.ContentValues;
+import android.content.Context;
+import android.database.Cursor;
+import android.database.sqlite.SQLiteDatabase;
+import android.database.sqlite.SQLiteOpenHelper;
+
+public class DatabaseHelper extends SQLiteOpenHelper {
+    public static final String DATABASE_NAME = "data.db";
+    public static final String TABLE_NAME = "data_table";
+    public static final String COL_1 = "ID";
+    public static final String COL_2 = "NAME";
+
+    public DatabaseHelper(Context context) {
+        super(context, DATABASE_NAME, null, 1);
+    }
+
+    @Override
+    public void onCreate(SQLiteDatabase db) {
+        db.execSQL("CREATE TABLE " + TABLE_NAME + "(ID INTEGER PRIMARY KEY AUTOINCREMENT, NAME TEXT)");
+    }
+
+    @Override
+    public void onUpgrade(SQLiteDatabase db, int oldVersion, int newVersion) {
+        db.execSQL("DROP TABLE IF EXISTS " + TABLE_NAME);
+        onCreate(db);
+    }
+
+    public boolean insertData(String name) {
+        SQLiteDatabase db = this.getWritableDatabase();
+        ContentValues contentValues = new ContentValues();
+        contentValues.put(COL_2, name);
+        long result = db.insert(TABLE_NAME, null, contentValues);
+        return result != -1;
+    }
+
+    public Cursor getAllData() {
+        SQLiteDatabase db = this.getWritableDatabase();
+        return db.rawQuery("SELECT * FROM " + TABLE_NAME, null);
+    }
+}
---
+++ app/src/main/java/com/example/tugas3/MainActivity.java
@@ -0,0 +1,76 @@
+package com.example.tugas3;
+
+import android.database.Cursor;
+import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
+import android.widget.EditText;
+import android.widget.Toast;
+import androidx.appcompat.app.AppCompatActivity;
+import androidx.recyclerview.widget.LinearLayoutManager;
+import androidx.recyclerview.widget.RecyclerView;
+import java.util.ArrayList;
+import java.util.List;
+
+public class MainActivity extends AppCompatActivity {
+
+    DatabaseHelper myDb;
+    RecyclerView recyclerView;
+    RecyclerViewAdapter adapter;
+    Button buttonAdd;
+    EditText editTextName;
+    List<String> dataList;
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+
+        myDb = new DatabaseHelper(this);
+
+        recyclerView = findViewById(R.id.recyclerView);
+        buttonAdd = findViewById(R.id.button_add);
+        editTextName = findViewById(R.id.editTextName);
+
+        recyclerView.setLayoutManager(new LinearLayoutManager(this));
+
+        loadData();
+
+        buttonAdd.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                addData();
+            }
+        });
+    }
+
+    private void loadData() {
+        dataList = new ArrayList<>();
+        Cursor res = myDb.getAllData();
+        if (res.getCount() == 0) {
+            Toast.makeText(this, "No data found", Toast.LENGTH_SHORT).show();
+            return;
+        }
+        while (res.moveToNext()) {
+            dataList.add(res.getString(1));  // Index 1 refers to NAME column
+        }
+        adapter = new RecyclerViewAdapter(dataList);
+        recyclerView.setAdapter(adapter);
+    }
+
+    private void addData() {
+        String name = editTextName.getText().toString();
+        if (name.isEmpty()) {
+            Toast.makeText(this, "Masukkan Nama", Toast.LENGTH_SHORT).show();
+            return;
+        }
+        boolean isInserted = myDb.insertData(name);
+        if (isInserted) {
+            Toast.makeText(this, "Data Berhasil Ditambahkan", Toast.LENGTH_SHORT).show();
+            editTextName.setText("");  // Clear input field
+            loadData();  // Refresh list after adding data
+        } else {
+            Toast.makeText(this, "Data Gagal Ditambahkan", Toast.LENGTH_SHORT).show();
+        }
+    }
+}
